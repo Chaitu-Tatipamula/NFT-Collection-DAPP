fc40bf09b8843133f5c14302def846f20f418d8d
durga chaitanya <chaitutatipamula023@gmail.com>
2023-10-05 15:19:14 +0530

    Changes in api

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
-  const imageUrl = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/"
+  const imageUrl = "https://raw.githubusercontent.com/Chaitu-Tatipamula/NFT-Collection-DAPP/main/my-app/public/cryptodevs/"
   console.log(tokenId);
   res.status(200).json({
     name : "Crypto-Dev NFT #" + tokenId , 
