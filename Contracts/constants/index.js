bf100f04edf3d06e7c8e9c5ecf06934c7cf4feda
durga chaitanya <chaitutatipamula023@gmail.com>
2023-10-05 14:53:19 +0530

    updates for nft metadata to work

--- Contracts/constants/index.js
+++ Contracts/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xCAf93300dad837513aD8049f3927C1f36a84fbd2"
-const METADATA_URL = ""
+const METADATA_URL = "https://nft-collection-dapp-three.vercel.app/api/"
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL}
 
